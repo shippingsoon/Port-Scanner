0a67996ffc933a5e7bd7d33144d058c0dadb0681
Shippingsoon <admin@shippingsoon.com>
2015-07-25 02:04:41 -0400

    added bash port scanner

---
+++ bash-scan.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USAGE="Usage: $0 HOST START_PORT END_PORT"
+
+if [ "$#" -ne 3 ]; then
+	echo $USAGE
+	exit
+fi
+
+if [ "`echo $2 | egrep ^[[:digit:]]+$`" = "" -o "`echo $3 | egrep ^[[:digit:]]+$`" = "" ]; then
+	echo $USAGE;
+	echo "Ports must be numberic"
+	exit
+fi
+
+#[ "$REPLY" -ge 1 -a "$REPLY" -le 32 ]
+if [ $2 -le 0 -o $2 -ge 65535 -o $3 -le 0 -o $3 -ge 65535 ]; then
+	echo $USAGE;
+	echo "Ports must be between 1 - 65534"
+	exit
+fi
+
+function portScanner () {
+	HOST=$1;
+	START_PORT=$2;
+	END_PORT=$3;
+
+	for PORT in `seq $START_PORT $END_PORT`; do
+		(echo >/dev/tcp/$HOST/$PORT) &>/dev/null
+		
+		if [ $? -eq 0 ]; then
+			echo "$HOST:$PORT is open"
+		else
+			echo "$HOST:$PORT is closed"
+		fi
+	done
+}
+
+portScanner $1 $2 $3
+
+#Copyright 2015 Shipping Soon
